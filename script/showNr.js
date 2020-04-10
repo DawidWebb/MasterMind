0c2857a7266e9cb808ac0486a539674c1d81a9fc
Tomek Matras <tomasz.matras@developerweb.pl>
2020-04-10 17:04:55 +0200

    change game play

--- script/showNr.js
+++ script/showNr.js
@@ -1,17 +1,15 @@
 class ShowNr extends Data {
   constructor(props) {
     super(props);
-    this.first = document.querySelector(".showNumbers__first");
-    this.sign1 = document.querySelector(".showNumbers__sign1");
-    this.second = document.querySelector(".showNumbers__second");
-    this.sign2 = document.querySelector(".showNumbers__sign2");
-    this.third = document.querySelector(".showNumbers__third");
-    document
+
+    this.headerBtn = document
       .querySelector(".header__btn")
       .addEventListener("click", this.randomNumbers.bind(this));
+    this.divs = document.querySelectorAll("picture__quiz");
   }
   randomNumbers() {
-    console.log("OK");
+    const numbers = Math.floor(Math.random(this.numbers) * this.numbers.length);
+    console.log(numbers);
   }
 }
 const showNr = new ShowNr();
